590543cb964b625d0c6ff53af3b770e8eb3d01d8
ZiadSala7 <zslah2800@gmail.com>
2024-08-28 20:20:18 +0300

    app images has been completed

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const String circle = 'assets/images/circle.png';
+  static const String doodleBg = 'assets/images/doodle_bg.png';
+  static const String splashDark = 'assets/images/splash_dark.png';
+  static const String splashLight = 'assets/images/splash_light.png';
+}

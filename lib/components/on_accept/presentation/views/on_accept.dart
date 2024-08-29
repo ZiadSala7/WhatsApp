7a3481cf42ce09b0abeffb33a12c6b4870cb8043
ZiadSala7 <zslah2800@gmail.com>
2024-08-29 13:56:47 +0300

    onaccept page body

--- lib/components/on_accept/presentation/views/on_accept.dart
+++ lib/components/on_accept/presentation/views/on_accept.dart
@@ -1,10 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:whats_app/constants.dart';
+import 'package:whats_app/core/utils/app_images.dart';
+import 'package:whats_app/core/utils/app_styles.dart';
 
 class OnAcceptPage extends StatelessWidget {
   const OnAcceptPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Color(0xff111B21),
+      body: OnAcceptPageBody(),
+    );
+  }
+}
+
+class OnAcceptPageBody extends StatelessWidget {
+  const OnAcceptPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Image.asset(
+              AppImages.circle,
+              color: editedGreen,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Text(
+                'Welcome To WhatsApp',
+                style: AppStyles.style25Bol.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: "Read our",
+                  style: AppStyles.style14Med.copyWith(
+                    color: const Color(0xff8696A0),
+                  ),
+                  children: [
+                    TextSpan(
+                      text: " Privacy Policy. ",
+                      style: AppStyles.style14Med.copyWith(
+                        color: const Color(0xff53BDEB),
+                      ),
+                    ),
+                    const TextSpan(
+                      text: "Tap 'Agree and Continue' to accept the",
+                      style: AppStyles.style14Med,
+                    ),
+                    TextSpan(
+                      text: " Terms of Services. ",
+                      style: AppStyles.style14Med.copyWith(
+                        color: const Color(0xff53BDEB),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 40.h,
+                width: 290.w,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: editedGreen,
+                    foregroundColor: const Color(0xff111B21),
+                    shape: const RoundedRectangleBorder(),
+                  ),
+                  child: const Text(
+                    'AGREE AND CONTINUE',
+                    style: AppStyles.style18Med,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 140),
+                child: InkWell(
+                  onTap: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.language,
+                        color: editedGreen,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'English',
+                        style: AppStyles.style16Med.copyWith(
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      const Icon(
+                        Icons.keyboard_arrow_down,
+                        color: editedGreen,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  // =============== fontSize 14
+  static const TextStyle style14Nor = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle style14Med = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style14Bol = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  // =============== fontSize 16
+  static const TextStyle style16Nor = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle style16Med = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style16Bol = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  // =============== fontSize 18
+  static const TextStyle style18Nor = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle style18Med = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style18Bol = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+  // =============== fontSize 20
+  static const TextStyle style20Nor = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle style20Med = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style20Bol = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  // =============== fontSize 25
+  static const TextStyle style25Nor = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle style25Med = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style25Bol = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+  );
+  // =============== fontSize 30
+  static const TextStyle style30Nor = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle style30Med = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle style30Bol = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+}

daa27545fae229e8eda9d5bc5225592d3a06ee2d
ZiadSala7 <zslah2800@gmail.com>
2024-08-29 12:12:36 +0300

    splash view

--- lib/components/splash/presentation/views/splash_view.dart
+++ lib/components/splash/presentation/views/splash_view.dart
@@ -1,10 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'package:whats_app/core/utils/app_images.dart';
-import 'package:whats_app/core/utils/app_router.dart';
+import 'package:whats_app/components/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -13,37 +8,6 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SplashViewBody(),
-    );
-  }
-}
-
-class SplashViewBody extends StatefulWidget {
-  const SplashViewBody({super.key});
-
-  @override
-  State<SplashViewBody> createState() => _SplashViewBodyState();
-}
-
-class _SplashViewBodyState extends State<SplashViewBody> {
-  late Timer timer;
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer(
-      const Duration(seconds: 5),
-      () {
-        GoRouter.of(context).pushReplacement(AppRouter.onAccept);
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        AppImages.splashLight,
-        width: 300.sp,
-      ),
     );
   }
 }
